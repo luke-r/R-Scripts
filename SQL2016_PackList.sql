a206231b7e7b25f3a719797e7f110e663c78a057
Luke R <luke.a.robin@gmail.com>
2017-01-16 10:13:15 -0600

    Update SQL2016_PackList.sql

--- SQL2016_PackList.sql
+++ SQL2016_PackList.sql
@@ -1,1 +1,17 @@
+USE [AdventureWorks2014];
+GO
+
+CREATE Procedure [dev].[R_Package_List] AS
+
+EXECUTE sp_execute_external_script  @language=N'R'  
+     ,@script = N'str(OutputDataSet);  
+     packagematrix <- installed.packages();  
+     NameOnly <- packagematrix[,1];  
+     OutputDataSet <- as.data.frame(NameOnly);'  
+     ,@input_data_1 = N'SELECT 1 as col'  
+     WITH RESULT SETS ((PackageName nvarchar(250) ))   
+
+
+GO
+
 
